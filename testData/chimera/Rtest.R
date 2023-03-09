7075674452ecf32f223303cae5de8f5a61ea0f5c
Stefan Lang <Stefan.Lang@med.lu.se>
2023-03-09 14:25:07 +0100

    Smaller test dataset

--- testData/chimera/Rtest.R
+++ testData/chimera/Rtest.R
@@ -1,13 +1,13 @@
 library(Seurat)
 library(reshape2)
-df = read.delim('testData/chimera/cell.csv',sep=",", header=F)
+df = read.delim('testData/chimera/cell2.csv',sep=",", header=F)
 mat = acast(df,V3 ~ V1,value.var='V5' ,fun.agg = max )
 mat[which(is.na(mat))] = 0
 cmd =  "target/release/chimera2sparse -i testData/chimera "
 print( cmd )
 system( cmd )
 
-data = Read10X( "testData/chimera/cell/filtered_feature_bc_matrix")
+data = Read10X( "testData/chimera/cell2/filtered_feature_bc_matrix")
 
 a = as.matrix(df)
 a = matrix(as.numeric(a), ncol=ncol(df))
